2d265fa673840a460c7f2d1382b78d8a9513c8d9
Joseph Yhu <xbhlx7@gmail.com>
2019-08-27 19:38:35 -0400

    Update

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # OOP PHP Game Show App
 4th Treehouse PHP Techdegree project.
-
-I changed the color for the win background and the color for the correct keys from green to blue.
--- inc/Game.php
+++ inc/Game.php
@@ -57,12 +57,12 @@ class Game
     public function key($letter)
     {
         if (!in_array($letter, $this->phrase->selected)) {
-            return "<input id='btn' type='submit' name='key' value='" . $letter . "' class='key'>";
+            return "<input type='submit' name='key' value='" . $letter . "' class='key'>";
         } else {
             if ($this->phrase->checkLetter($letter)) {
-                return "<input id='btn' type='submit' name='key' value='" . $letter . "' class='key correct' disabled>";
+                return "<input type='submit' name='key' value='" . $letter . "' class='key correct' disabled>";
             } else {
-                return "<input id='btn' type='submit' name='key' value='" . $letter . "' class='key incorrect' disabled>";
+                return "<input type='submit' name='key' value='" . $letter . "' class='key incorrect' disabled>";
             }
         }
     }
@@ -153,4 +153,3 @@ class Game
         }
     }
 }
-echo "<script src='../js/script.js'></script>";
