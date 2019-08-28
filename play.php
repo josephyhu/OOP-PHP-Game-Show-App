bf7bdb3f804df6e4c0de7338a9cae6c0a152cfaf
Joseph Yhu <xbhlx7@gmail.com>
2019-08-28 07:17:25 -0400

    Update play.php

--- play.php
+++ play.php
@@ -44,57 +44,57 @@ $game = new Game($phrase);
     <script>
     function myFunction(event) {
     var x = event.key;
-    if (x == 'a') {
+    if (x == 'a' || x == 'A') {
     document.getElementById('a').click();
-  } else if (x == 'b') {
+  } else if (x == 'b' || x == 'B') {
     document.getElementById('b').click();
-  } else if (x == 'c') {
+  } else if (x == 'c' || x == 'C') {
     document.getElementById('c').click();
-  } else if (x == 'd') {
+  } else if (x == 'd' || x == 'D') {
     document.getElementById('d').click();
-  } else if (x == 'e') {
+  } else if (x == 'e' || x == 'E') {
     document.getElementById('e').click();
-  } else if (x == 'f') {
+  } else if (x == 'f' || x == 'F') {
     document.getElementById('f').click();
-  } else if (x == 'g') {
+  } else if (x == 'g' || x == 'G') {
     document.getElementById('g').click();
-  } else if (x == 'h') {
+  } else if (x == 'h' || x == 'H') {
     document.getElementById('h').click();
-  } else if (x == 'i') {
+  } else if (x == 'i' || x == 'I') {
     document.getElementById('i').click();
-  } else if (x == 'j') {
+  } else if (x == 'j' || x == 'J') {
     document.getElementById('j').click();
-  } else if (x == 'k') {
+  } else if (x == 'k' || x == 'K') {
     document.getElementById('k').click();
-  } else if (x == 'l') {
+  } else if (x == 'l' || x == 'L') {
     document.getElementById('l').click();
-  } else if (x == 'm') {
+  } else if (x == 'm' || x == 'M') {
     document.getElementById('m').click();
-  } else if (x == 'n') {
+  } else if (x == 'n' || x == 'N') {
     document.getElementById('n').click();
-  } else if (x == 'o') {
+  } else if (x == 'o' || x == 'O') {
     document.getElementById('o').click();
-  } else if (x == 'p') {
+  } else if (x == 'p' || x == 'P') {
     document.getElementById('p').click();
-  } else if (x == 'q') {
+  } else if (x == 'q' || x == 'Q') {
     document.getElementById('q').click();
-  } else if (x == 'r') {
+  } else if (x == 'r' || x == 'R') {
     document.getElementById('r').click();
-  } else if (x == 's') {
+  } else if (x == 's' || x == 'S') {
     document.getElementById('s').click();
-  } else if (x == 't') {
+  } else if (x == 't' || x == 'T') {
     document.getElementById('t').click();
-  } else if (x == 'u') {
+  } else if (x == 'u' || x == 'U') {
     document.getElementById('u').click();
-  } else if (x == 'v') {
+  } else if (x == 'v' || x == 'V') {
     document.getElementById('v').click();
-  } else if (x == 'w') {
+  } else if (x == 'w' || x == 'W') {
     document.getElementById('w').click();
-  } else if (x == 'x') {
+  } else if (x == 'x' || x == 'X') {
     document.getElementById('x').click();
-  } else if (x == 'y') {
+  } else if (x == 'y' || x == 'Y') {
     document.getElementById('y').click();
-  } else if (x == 'z') {
+  } else if (x == 'z' || x == 'Z') {
     document.getElementById('z').click();
   }
   }
